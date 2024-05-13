29a7b615aa94869cb1a1a3cf80810878d84cf6ac
RobottiFranco <franco.robotti0@gmail.com>
2024-05-13 20:11:22 -0300

    se agregan test

--- Parcial1/src/test/java/uy/edu/ucu/aed/Parcial1Test_Junit5.java
+++ Parcial1/src/test/java/uy/edu/ucu/aed/Parcial1Test_Junit5.java
@@ -21,14 +21,19 @@ public class Parcial1Test_Junit5 {
         instanceVariable = null;
     }
 
-    /**
-     * Sample test in JUnit 5
-     */
     @Test
     public void ltimArbolVacio() {
         TArbolDeProductos arbol = new TArbolDeProductos();
 
         assertEquals(-1, arbol.ltim());
+    }
+
+    public void ltimArbol1Elemento() {
+        TArbolDeProductos arbol = new TArbolDeProductos();
+        Producto nuevoProducto = new Producto(1, "instanceVariable");
+        arbol.insertar(nuevoProducto.getIdentificador(), nuevoProducto);
+
+        assertEquals(0, arbol.ltim());
     }
 
     @Test
@@ -41,5 +46,50 @@ public class Parcial1Test_Junit5 {
         arbol.insertar(nuevoProducto.getIdentificador(), nuevoProducto);
 
         assertEquals(0.5, arbol.ltim());
+    }
+
+    @Test
+    public void ltimArbol3lemento() {
+        TArbolDeProductos arbol = new TArbolDeProductos();
+        Producto nuevoProducto = new Producto(1, "instanceVariable");
+        arbol.insertar(nuevoProducto.getIdentificador(), nuevoProducto);
+
+        nuevoProducto = new Producto(2, "instanceVariable");
+        arbol.insertar(nuevoProducto.getIdentificador(), nuevoProducto);
+
+        arbol.insertar(4, new Producto(4, null));
+
+        assertEquals(1, arbol.ltim());
+    }
+
+    @Test
+    public void ltimArbol4Elemento() {
+        TArbolDeProductos arbol = new TArbolDeProductos();
+        Producto nuevoProducto = new Producto(1, "instanceVariable");
+        arbol.insertar(nuevoProducto.getIdentificador(), nuevoProducto);
+
+        nuevoProducto = new Producto(2, "instanceVariable");
+        arbol.insertar(nuevoProducto.getIdentificador(), nuevoProducto);
+
+        arbol.insertar(4, new Producto(4, null));
+        arbol.insertar(8, new Producto(8, null));
+
+        assertEquals(1.5, arbol.ltim());
+    }
+
+    @Test
+    public void testLongitudTrayectoriaInternaMediaArbolConocido() {
+        TArbolDeProductos arbol = new TArbolDeProductos();
+
+        arbol.insertar(2, new Producto(2, null));
+        arbol.insertar(5, new Producto(5, null));
+        arbol.insertar(3, new Producto(3, null));
+        arbol.insertar(1, new Producto(1, null));
+        arbol.insertar(9, new Producto(9, null));
+        arbol.insertar(4, new Producto(4, null));
+        arbol.insertar(8, new Producto(8, null));
+        arbol.insertar(6, new Producto(6, null));
+        arbol.insertar(7, new Producto(7, null));
+        assertEquals(21D / 9D, arbol.ltim());
     }
 }
